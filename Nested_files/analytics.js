f9b3fef5b4691f50d80de888adfa120f71c349c5
Raelian Movement <Raelian Movement>
2016-08-08 05:25:40 -0500

    Blindly copied the Nested webpage with Firefox.

---
+++ Nested_files/analytics.js
@@ -0,0 +1,39 @@
+(function() {
+var ozoki_tc = 'P6JyxJwt2E4RCDwPVw5cWkwJ.gDUyxhm';
+var ozoki_os = 's.tworismo.com';
+var ozoki_url = 'https://s.tworismo.com/2/743173/analytics.js?pp=Quantcast&amp;ac=243091&amp;pc=5181503&amp;cr=37045603&amp;dm=120x600&amp;cb=307621976&amp;si=22061&amp;pi=74076&amp;dt=7431731421771969641003';
+
+var ozoki_ct = {"pp":"Quantcast", "ac":"243091", "pc":"5181503", "cr":"37045603", "dm":"120x600", "cb":"307621976", "si":"22061", "pi":"74076", "dt":"7431731421771969641003"};
+
+if (typeof ozoki_ct == "undefined" || ozoki_ct == null)  {
+var ozoki_ct = {};
+}
+ozoki_ct.ci = '743173';
+
+var ozoki_opt = {sm:49};
+
+ozoki_opt.mouse=true;
+ozoki_opt.dup=true;
+ozoki_opt.wrip = true;
+
+
+
+
+
+
+
+
+
+
+
+var PAGESPEED_VERSION = "4.14.9";
+
+
+
+
+(function() {
+!function(){!function(){var o="2";if(window.ozoki_data){if(!ozoki_opt.dup)return}else window.ozoki_data=[];var i={};i.ozoki_st=(new Date).getTime(),i.ozoki_os=ozoki_os,"undefined"!=typeof ozoki_fl&&(i.ozoki_fl=ozoki_fl),i.ozoki_url=ozoki_url,i.ozoki_ct=ozoki_ct,i.ozoki_tc=ozoki_tc,i.ozoki_opt=ozoki_opt,window.ozoki_data.push(i);var t,e=ozoki_url.substr(0,ozoki_url.indexOf(":"))+"://"+ozoki_os+"/"+o+"/"+PAGESPEED_VERSION+"/",_=!1,d=function(){_||(document&&!t&&(t=document.createElement("script"),t.src=e+"loaded.js"),document.body?(_=!0,document.body.appendChild(t)):window.setTimeout(d,4))};d()}()}();}());
+
+
+}());
+

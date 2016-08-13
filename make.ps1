72695631d4adbc49330535057ab0fbec766255f5
Raelian Movement <irwin@DESKTOP-FHAENV2.(none)>
2016-08-13 03:12:19 -0500

    Created a folder structure and moved everything.

--- make.ps1
+++ make.ps1
@@ -1,7 +1,10 @@
+cd coffee/
+
 echo 'Preprocessing...'
-coffeescript-concat -I . nestedscript.coffee -o nestedscript.preprocessed.coffee
+coffeescript-concat -I . nestedscript.coffee -o compiled/nestedscript.preprocessed.coffee
 
 echo 'Compiling...'
+cd compiled/
 coffee -bc nestedscript.preprocessed.coffee
 
 echo 'Cleaning up...'
@@ -9,3 +12,6 @@ rm nestedscript.preprocessed.coffee
 
 # -force means 'overwrite if needed'
 mv -force nestedscript.preprocessed.js nestedscript.js
+
+cd ..
+cd ..
--- make.sh
+++ make.sh
@@ -1,4 +1,17 @@
-coffeescript-concat -I . nestedscript.coffee -o nestedscript.preprocessed.coffee
+cd coffee/
+
+echo 'Preprocessing...'
+coffeescript-concat -I . nestedscript.coffee -o compiled/nestedscript.preprocessed.coffee
+
+echo 'Compiling...'
+cd compiled/
 coffee -bc nestedscript.preprocessed.coffee
-mv nestedscript.preprocessed.js nestedscript.js
+
+echo 'Cleaning up...'
 rm nestedscript.preprocessed.coffee
+
+# -force means 'overwrite if needed'
+mv --force nestedscript.preprocessed.js nestedscript.js
+
+cd ..
+cd ..
